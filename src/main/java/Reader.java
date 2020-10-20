ceb81688e25540aa2bf13ab79ce79853229ba71e
Cruzanio Villarreal <cruzanio.villarreal@gmail.com>
2020-10-20 12:56:48 -0500

    imported StringUtils, utilizing: isNumeric, swapCase, reverse methods

--- src/main/java/Reader.java
+++ src/main/java/Reader.java
@@ -1,25 +1,20 @@
 import org.apache.commons.lang3.StringUtils;
-
 import java.util.List;
 import java.util.Scanner;
 
 public class Reader {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter a phrase: ");
         String answer = scanner.nextLine();
         if (!StringUtils.isNumeric(answer)) {
             System.out.println(answer+" is not a number.");
         }
+        System.out.println("Enter a phrase: ");
         String answer2 = scanner.nextLine();
-        String[] split = StringUtils.split(answer2, " ");
-        String[] toJoin = new String[split.length];
-        for (int i = 0; i < split.length; i++) {
-            String word = split[i];
-            String cappedFirst = word.substring(0, 1).toUpperCase() + word.substring(1);
-            toJoin[i] = cappedFirst;
-        }
-
-        System.out.println(StringUtils.isNumeric(scanner.nextLine()));
-        System.out.println(StringUtils.isNumeric(scanner.nextLine()));
+        System.out.println(StringUtils.swapCase(answer2));
+        System.out.println("Enter a phrase to reverse: ");
+        String answer3 = scanner.nextLine();
+        System.out.println(StringUtils.reverse(answer3));
     }
 }
